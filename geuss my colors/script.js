c6680eea2fde96c15b398ef4277ee6a34e16498a
AamnaNazMuzaffar <aamnanazmuzaffar@gmail.com>
2023-08-30 01:26:13 +0500

    int

---
+++ geuss my colors/script.js
@@ -0,0 +1,383 @@
+"use strict";
+
+// let x = 2+4;
+// x += 4
+// x *= 2
+// x ++
+// x--
+// x -= 9
+// x --
+// x /= 5
+// console.log(x)
+
+// let y,x;
+
+// y = x= 4+5;
+
+// console.log(y)
+// console.log(x)
+
+// const markWeight = 78;
+// const markHight = 1.69;
+
+// const johnWeight = 92;
+// const johnHight = 1.95;
+
+// const bmiMark =markWeight/markHight**2 ;
+// const bmiJohn = johnWeight/(johnHight*johnHight);
+// console.log(bmiMark,bmiJohn)
+
+// const bmi =bmiMark > bmiJohn;
+// console.log(bmi)
+
+//Data 2: Marks weights 95 kg and is 1.88 m tall. John weights 85 kg and is 1.76
+//m tall.
+
+// const markWeight = 98;
+// const markHight = 1.88;
+// const johnWeight = 85;
+// const johnHight = 1.76;
+
+// const bmiMark =markWeight/markHight**2 ;
+// const bmiJohn = johnWeight/(johnHight*johnHight);
+// console.log(bmiMark,bmiJohn)
+
+// const bmi =bmiMark > bmiJohn;
+// console.log(bmi)
+
+// const markWeight = 98;
+// const markHight = 1.88;
+
+// const johnWeight = 86;
+// const johnHight = 1.76;
+
+// const bmiMark =markWeight/markHight**2 ;
+// const bmiJohn = johnWeight/(johnHight*johnHight);
+
+// if (bmiMark > bmiJohn){
+// console.log(`Mark's BMI (${bmiMark}) is higher than John's ${bmiJohn} bmi!`)
+// }
+// else{
+// console.log(`john's BMI ${bmiJohn} is higher than mark's (${bmiMark}) bmi!`)
+
+// }
+
+// const mony = 1;
+// if(mony)
+// {
+//   console.log(`you have ${mony} rupes.`)
+
+// }else{
+//   console.log(`you have no mony`)
+// }
+
+// console.log(275/100*20)
+// console.log(275/100*15)
+
+// const  bill = 275;
+// const  calculation // console.log(275/100*20)
+// console.log(275/100*15)
+
+// const  bill = 275;
+// const  calculation = bill >= 50 && bill <= 300 ? bill/100*15 :bill/100*20  ;
+// const total = bill + calculation;
+// console.log(`The bill was ${bill}, the tip was ${calculation}, and the total value
+// ${total}`)
+// = bill >= 50 && bill <= 300 ? bill/100*15 :bill/100*20  ;
+// const total = bill + calculation;
+// console.log(`The bill was ${bill}, the tip was ${calculation}, and the total value
+// ${total}`)
+
+// console.log(430/100*20)
+// console.log(430/100*15)
+
+// const  bill = 430;
+// const  calculation = bill >= 50 && bill <= 300 ? bill/100*15 :bill/100*20  ;
+// const total = bill + calculation;
+// console.log(`The bill was ${bill}, the tip was ${calculation}, and the total value
+// ${total}`)
+
+// console.log(40/100*20)
+// console.log(40/100*15)
+
+// const  bill = 40;
+// const  calculation = bill >= 50 && bill <= 300 ? bill/100*15 :  bill/100*20;
+// const total = bill + calculation;
+// console.log(`The bill was ${bill}, the tip was ${calculation}, and the total value
+// ${total}`)
+
+// const dolphins = (96 +108 + 89) / 3;
+// const Koalas = (88+ 91 + 110) / 3;
+// console.log(dolphins)
+// console.log(Koalas)
+
+// if( dolphins > Koalas ){
+//     console.log(`dolphins are win`)
+// }else if(Koalas > dolphins ){
+//     console.log(`koalas are win`)
+// }
+// else if(dolphins===Koalas ){
+//     console.log(`both are win`)`)
+// }else if(Koalas > dolphins  && Koalas > 100){
+//     console.log(`koalas are win`)
+// }
+// else if(dolphins===Koalas && dolphins > 100 && Koalas > 100 ){
+//     console.log(`both are win`)
+// }else{
+//     console.log(`both are fail
+// }
+
+// const dolphins = (97+ 112 + 101) / 3;
+// const Koalas = (109+ 95 +123) / 3;
+// console.log(dolphins)
+// console.log(Koalas)
+
+// if( dolphins > Koalas && dolphins > 100){
+//     console.log(`dolphins are win`)
+// }else if(Koalas > dolphins  && Koalas > 100){
+//     console.log(`koalas are win`)
+// }
+// else if(dolphins===Koalas && dolphins > 100 && Koalas > 100 ){
+//     console.log(`both are win`)
+// }else{
+//     console.log(`both are fail`)
+// }
+
+// const dolphins = (98 + 112 + 101) / 3;
+// const Koalas = (98 + 112 + 101) / 3;
+// console.log(dolphins)
+// console.log(Koalas)
+
+// if (dolphins === Koalas && dolphins > 100 && Koalas > 100) {
+//   console.log(`the match is draw`);
+// } else {
+//   console.log(`both are fail`);
+// }
+
+// function fruitProcessor (apples,oranges){
+//     const juice = `juice with ${apples} apples and ${oranges} oranges`;
+//     return juice;
+// }
+// const applesJuice = fruitProcessor(5,7)
+// console.log(applesJuice)
+// const applesAndJuice = fruitProcessor(4,8)
+// console.log(applesAndJuice)
+// console.log(fruitProcessor(9,2));
+
+// function calcAge1( birthyear){
+//     return 2023 - birthyear;
+
+// }
+
+// const age1 = calcAge1(2006)
+
+// const calcAge2= function ( birthyear){
+//     return 2023 - birthyear;
+
+// }
+// const age2 = calcAge2(2006)
+// console.log(age1,age2)
+// function fruitCutting(fruit){
+//     return fruit *4
+// }
+
+// function fruitProcessor (apples,oranges){
+
+//     const applePieces = fruitCutting(apples)
+//     const orangePieces = fruitCutting(oranges)
+//     const juice = `juice with ${applePieces} apples and ${orangePieces} oranges`;
+//     return juice;
+// }
+// console.log(fruitProcessor(2,3))
+
+// const calcAverage = (a, b, c) => (a + b + c) / 3;
+
+// let dolphinsScore = calcAverage(44, 23, 71);
+// let KoalasScore = calcAverage(65, 54, 49);
+
+// console.log(dolphinsScore, KoalasScore);
+
+// const calculate = function (avadophin, avakoalas) {
+//   if
+
+//   (avadophin >= 2 * avakoalas) {
+//     console.log(`dolphins are win (${avadophin} vs ${avakoalas})`);
+//   }
+
+//   else if (avakoalas >= 2 * avadophin) {
+//     console.log(`koalas are win (${avakoalas} vs ${avadophin})`);
+//   }
+
+//   else {
+//     console.log(`no one wins (${avakoalas} vs ${avadophin})`);
+//   }
+// };
+// calculate(dolphinsScore, KoalasScore);
+
+// dolphinsScore = calcAverage(85,54,41.);
+//  KoalasScore = calcAverage(23,34,27);
+// console.log(dolphinsScore,KoalasScore)
+// calculate(dolphinsScore,KoalasScore)
+
+// console.log((275 / 100) * 20);
+// console.log((275 / 100) * 15);
+
+// // const bill = 275;
+// const tipArray = []
+// const calculateTip = function (bill) {
+//   const fifteenPercentTipCondition = bill >= 50 && bill <= 300;
+//   let tip;
+//   if (fifteenPercentTipCondition) {
+//     tip = (bill / 100) * 15;
+//   } else {
+//     tip = (bill / 100) * 20;
+//   }
+//   tipArray.push(tip)
+//   const total = bill + tip;
+//   console.log(`The bill was ${bill}, the tip was ${tip}, and the total value
+// ${total}`);
+// };
+
+// calculateTip(232);
+// calculateTip(2332);
+// calculateTip(200);
+// console.log('tipArray: ',tipArray
+
+// const amna = {
+//   firstName: prompt("firstName"),
+//   lastName: prompt("lastName"),
+//   birthyear: Number(prompt("birthyear")),
+//   age: function () {
+//     this.aage = 2023 - this.birthyear;
+//     return this.aage;
+//   },
+// license : false,
+
+//   summary: function () {
+//     return ` my firstName is ${this.firstName}.
+// and my lastName is ${this.lastName}.
+// I,m ${this.age()} year old.
+// because i was born in ${this.birthyear}.
+// i have ${this.license ? "a": "no"} driver license.`;
+//   },
+// };
+// console.log(amna.summary())
+
+// const markBMI = {
+//   fullName: "mark",
+//   mass: 78,
+//   height: 1.69,
+//   bmi: function () {
+//     return this.mass / this.height ** 2;
+//   },
+// };
+
+//92 kg and is 1.95 m
+
+// const johnBMI = {
+//   fullName: "john",
+//   mass: 92,
+//   height: 1.95,
+//   bmi: function () {
+//     return this.mass / this.height ** 2;
+//   },
+// };
+// const heighter = markBMI.bmi() > johnBMI.bmi();
+
+// function func(heighter) {
+
+//   if (heighter) {
+//     console.log(
+//       `mark,s BMI is highter (${markBMI.bmi()}) than john BMI ${johnBMI.bmi()} `
+//     );
+//   } else  {
+//     console.log(
+//       `john,s BMI is highter (${johnBMI.bmi()}) than mark BMI ${markBMI.bmi()} `
+//     );
+//   }
+// }
+// func(heighter);
+
+// let amna = [
+//   "amna",
+//   "muzaffar",
+//   2023 - 2006,
+//   ["AAA", "BBB", "CCC"],
+//   false,
+//   "Good Girl",
+// ];
+// const array = [];
+
+// for (let i = 0 ;i < amna.length; i++){
+//   console.log("it,s " ,amna[i],typeof amna[i],array[i])
+
+// array.push(typeof amna[i])
+// }
+// console.log(array);
+
+// const years = [2000,2001,2003,2004,2005,2006]
+// const ages  = []
+// for(let i = 0;i < years.length; i++ ){
+//   ages.push( 2023- years[i]  );
+// }
+// console.log(ages)
+
+// for (let i = amna.length - 1; i >= 0; i--) {
+//   console.log(amna[i]);
+// array.push(typeof amna[i])
+// }
+// console.log(array);
+
+// let rep = Math.trunc(Math.random() * 6 ) +1;
+// while(rep !== 6 ){
+//   console.log(`you rolled dice ${rep}`);
+//   rep = Math.trunc(Math.random() * 6 ) +1;
+//   if (rep === 6){
+//     console.log(`you rolled ${rep}`)
+//   }
+// }
+
+//  const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86 ,52];
+//  const total =[]
+//  const tips = []
+// for(let i = 0 ;i < bills.length; i++){
+
+//  function calc(bill){
+
+//     let tipCalculation = bill >= 50 && bill <= 300 ? (bill/100)*15:(bill/100)*20;
+// tips.push(tipCalculation)
+// total.push(bills[i]+ tipCalculation)
+
+//  }
+//  calc(bills[i])
+//  console.log(`the bill is  {${bills [i]}}.
+// the tip is  (${tips}).
+// the total is  [${total}]. `)
+
+// }
+// console.log(tips)
+// console.log(total)
+// function calcAverage(arr){
+// let sum = 0
+// for(let i = 0 ; i < arr.length;i++){
+//     sum += arr[i]
+// }
+// return sum/ arr.length;
+// }
+// console.log( calcAverage([2,3,7]))
+
+const button = document.querySelector("button");
+const bulb = document.querySelector(".bulb");
+const h1 = document.querySelector("h1");
+
+function colors() {
+  const r = Math.trunc(Math.random() * 255);
+  const g = Math.trunc(Math.random() * 255);
+  const b = Math.trunc(Math.random() * 255);
+
+  return `rgb(${r}, ${g}, ${b})`
+}
+
+button.addEventListener("click", function () {
+  bulb.style.backgroundColor = colors();
+});
